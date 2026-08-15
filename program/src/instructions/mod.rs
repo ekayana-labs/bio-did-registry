ea1341d9b797f5301b4fa1c6d12a2928401bc9c0
Suraj Kumar <suraj410401@gmail.com>
2026-08-15 22:11:28 +0530

    Add workspace manifests and program entrypoint

---
+++ program/src/instructions/mod.rs
@@ -0,0 +1,10 @@
+pub mod shared;
+
+pub mod add_service;
+pub mod add_verification_method;
+pub mod deactivate;
+pub mod initialize;
+pub mod remove_service;
+pub mod remove_verification_method;
+pub mod set_controllers;
+pub mod set_verification_method_flags;
